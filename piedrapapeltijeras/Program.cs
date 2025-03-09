d2f330f92bb825c94ebc9b369326d4747cd2904b
Julio <jantram710@g.educaand.es>
2025-03-09 15:55:34 +0100

    todo

---
+++ piedrapapeltijeras/Program.cs
@@ -0,0 +1,19 @@
+using piedrapapeltijeras.game;
+using piedrapapeltijeras.participants;
+
+class Program
+{
+    static void Main()
+    {
+        Participant player1 = new Participant("Player 1");
+        Participant player2 = new Participant("Player 2");
+        
+        int rounds = 5;
+        
+        Game game = new Game(player1, player2, rounds);
+
+        game.Play();
+        
+        Console.WriteLine("La partida ha finalizado");
+    }
+}
---
+++ piedrapapeltijeras/participants/Participant.cs
@@ -0,0 +1,23 @@
+namespace piedrapapeltijeras.participants;
+
+public class Participant
+{
+    
+    public string Name { get; set; }
+    private List<string> _options = new List<string>{"rock", "paper", "scissors"};
+
+
+    public Participant(string name)
+    {
+        Name = name;
+    }
+    
+
+    public string GetRandomOption()
+    {
+        Random random = new Random();
+        int index = random.Next(_options.Count);
+        return _options[index];
+    }
+    
+}
